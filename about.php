e2d4cdccefef7125411dbb026d60b1e6b9fefc36
whoisjuarez <133733073+whoisjuarez@users.noreply.github.com>
2023-06-06 21:44:25 -0400

    Initial files sent
    index.php, about.php, folder: img, css and js.

---
+++ about.php
@@ -0,0 +1,208 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>André Cardoso - Final Project</title>
+
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Big+Shoulders+Display:wght@100;300;400;700;900&family=IBM+Plex+Sans:wght@100;300;400;700&display=swap" rel="stylesheet">
+
+	<link rel="stylesheet" href="css/style-about.css">
+</head>
+<body>
+	<section class="hero" id="begin">
+		<div class="container">
+			<div class="text-box">
+				<h1>Who is Juarez?</h1>
+				<h2><span class="no-decoration">One man </span><span class="decoration">Web & Design</span><span class="no-decoration"> Studio by</span><span class="no-decoration"><strong> André Cardoso</strong></span></h2>
+				<h3><span class="no-decoration">Montreal, Canada</span></h3>
+			</div>
+		</div>
+	</section>
+
+	<!-- STICKY NAV -->
+	<nav class="nav-bg sticky">
+            <a href="#begin">Begin</a>
+            <a href="#see">See</a>
+            <a href="#read">Read</a>
+            <a href="#know">Know</a>
+            <a href="#talk">Talk</a>
+	</nav>
+
+	<!-- PORTIFOLIO -->
+	<section class="port" id="see">
+		<div class="container">
+			<h4 data-aos="fade-in">See</h4>
+
+			<ul data-aos="fade-in">
+				<li><a href="#see">All</a></li>
+				<li><a href="#see">Branding</a></li>
+				<li><a href="#see">Packaging</a></li>
+				<li><a href="#see">Animation</a></li>
+				<li><a href="#see">Presentation</a></li>
+				<li><a href="#see">Print</a></li>
+			</ul>
+			
+			<div class="port-flex">
+                <div class="port-left">
+                    <div class="card">
+                        <div class="port-1 blue-filter">
+                            <a href=""><img src="img/urbinz.jpg" alt="Branding - Urbinz"></a>
+                        </div>
+			    	</div> <!-- END OF CARD -->
+				
+                    <div class="card">
+                        <div class="port-2 blue-filter">
+                            <a href=""><img src="img/cn2.jpg" alt="Branding - College National"></a> 
+                        </div>
+                    </div><!-- END OF CARD -->
+                </div>
+                                    
+                <div class="port-middle">
+                    <div class="card">
+                        <div class="port-3 blue-filter">
+                             <a href=""><img src="img/anim.jpg" alt="Branding - College National"></a>
+                         </div>
+                    </div>  
+
+                    <div class="card">
+                         <div class="port-4 blue-filter">
+                              <a href=""><img src="img/pack.jpg" alt="Branding - College National"></a> 
+                        </div>
+                    </div><!-- END OF CARD -->
+                </div>
+                   	<!-- END OF CARD -->
+
+				
+                <div class="port-right">
+                    <div class="card">
+                        <div class="port-5 blue-filter">
+                            <a href=""><img src="img/cn.jpg" alt="Branding - College National"></a> 
+                        </div>
+                    </div><!-- END OF CARD -->
+
+                    <div class="card">
+                        <div class="port-6 blue-filter">
+                            <a href=""><img src="img/edenred.jpg" alt="Branding - College National"></a> 
+                        </div>
+                    </div><!-- END OF CARD -->
+                </div>
+		    </div>
+	<a id="read"></a><!-- ANCHOR RESUME SECTION-->
+	</section>
+
+	<!-- RESUME -->
+	<section class="resume">
+		<div class="container">
+			<h4>Read</h4>
+			<div class="generic-flex">
+				<div class="left">
+					<h2><span class="decoration">Experience</span></h2>
+					<div class="content">
+						<h5><i class="fa-regular fa-calendar-xmark"></i>May 2021 - Jun 2022</h5>
+						<h6>Collège National</h6>
+						<p>Marketing Designer</p>
+					</div>
+
+					<div class="content">
+						<h5><i class="fa-regular fa-calendar-xmark"></i>Dec 2015 - May 2021</h5>
+						<h6>Smolarz Design</h6>
+						<p>Packaging Designer</p>
+					</div>
+
+					<div class="content">
+						<h5><i class="fa-regular fa-calendar-xmark"></i>Jan 2015 - Jul 2015</h5>
+						<h6>Meu Estúdio</h6>
+						<p>PowerPoint Presentation Designer</p>
+					</div>
+
+					<div class="content">
+						<h5><i class="fa-regular fa-calendar-xmark"></i>Nov 2011 - Jun 2014</h5>
+						<h6>La Gracia Design</h6>
+						<p>PowerPoint Presentation Designer</p>
+					</div>
+				</div><!-- END OF RIGHT CARD -->
+
+				<div class="right">
+					<h2><span class="no-decoration">Education</span></h2>	
+
+					<div class="content">
+						<h5><i class="fa-regular fa-calendar-plus"></i>Mar 2023 - Present</h5>
+						<h6>Website Design Specialist</h6>
+						<p>ACS - Vanier College, Montreal</p>
+					</div>	
+
+					<div class="content">
+						<h5><i class="fa-regular fa-calendar-check"></i>Feb 2004 - Dec 2007</h5>
+						<h6>Advertising & Publicity</h6>
+						<p>Bachelor - Brazil</p>
+					</div>
+
+					<div class="content">
+						<h5><i class="fa-regular fa-calendar-check"></i>Feb 2000 - Dec 2001</h5>
+						<h6>Admin, Commerce & IT</h6>
+						<p>DEP - Brazil</p>
+					</div>
+				</div> <!-- END OF LEFT CARD -->
+			</div>
+		</div>
+	<a id="know"></a><!-- ANCHOR ABOUT SECTION-->
+	</section>
+
+	<!-- ABOUT -->
+	<section class="about">
+		<div class="container">
+			<h4>Know</h4>
+			<div class="generic-flex">
+				<div class="photo" alt="Me"></div><!--  end of card -->
+
+				<div class="content">
+					<h2><span class="no-decoration">Who is Juarez?</span></h2>
+					<h3><span class="decoration"><strong>André Cardoso</strong></span></h3>
+					
+					<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+					<p>Voluptate, ipsam laborum vero illo maiores, nihil quia quae labore deserunt doloremque magni cum debitis, excepturi earum esse saepe iusto. Ea, aspernatur?</p>
+
+					<div>
+						<a href="" class="btn" alt="Hire me">Hire me</a>
+						<a href="" class="btn" alt="Download CV">Download full CV</a>
+					</div>
+					
+				</div><!--  end of card -->
+			</div>
+		</div>
+	</section>
+
+	<!-- FOOTER -->
+	<footer id="talk">
+		<div class="container">
+			<div class="contact">
+				<h4>Talk</h4>
+				<h3>Email</h3>
+				<a href="mailto:whoisjuarez@gmail.com" alt="Email">whoisjuarez@gmail.com</a>
+	
+				<h3>Phone</h3>
+				<a href="tel:+1-438-926-1911" alt="Phone"">+1 438 926.1911</a>
+	
+				<h3>Address</h3>
+				<p>Canada</p>
+				<p>Montreal, QC H2L 2Y3</p>
+				<p>5-2250 Plessis</p>
+			</div>
+
+			<div class="social">
+				<a href="https://www.instagram.com/whoisjuarez/" target="_blank"><i class="fa-brands fa-instagram"></i></a>
+				<a href="https://www.linkedin.com/in/andr%C3%A9-cardoso-038126243/" target="_blank"><i class="fa-brands fa-linkedin"></i></a>
+				<a href="https://vimeo.com/whoisjuarez" target="_blank"><i class="fa-brands fa-vimeo-v"></i></a>
+				<a href="https://prideatwork.ca/" target="_blank"><i class="fa-solid fa-mars-double"></i></a>
+			</div>
+
+			<p><span class="decoration">Template by who?</span></p>
+		</div>
+	</footer>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>André Cardoso - Final Project</title>
+
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Big+Shoulders+Display:wght@100;300;400;700;900&family=IBM+Plex+Sans:wght@100;300;400;700&display=swap" rel="stylesheet">
+
+	<link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+
+	<link rel="stylesheet" href="css/style-home.css">
+
+	<script src="js/main.js" defer></script>
+</head>
+<body>
+	<section class="hero" id="begin">
+		<div class="container">
+			<h1>Who is Juarez?</h1>
+			<div class="text-box">
+				<h2><span class="no-decoration">One man </span><span class="decoration">Web & Design</span><span class="no-decoration"> Studio by</span><span class="no-decoration"><strong> André Cardoso</strong></span></h2>
+				<h3><span class="no-decoration">Montreal, Canada</span></h3>
+			</div>
+		</div>
+	</section>
+
+    <a href="about.php" class="btn">Come on in and know more about me and my work <i class="fa-solid fa-arrow-right-long"></i></a>
+</body>
+</html>
